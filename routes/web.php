70415bda809a3b240cf071145e16f7ac1843d51a
Leandro Arturi <lea.arturi@gmail.com>
2023-11-27 20:35:48 -0300

    Route admin dashboard subscribers

--- routes/web.php
+++ routes/web.php
@@ -23,10 +23,6 @@ Route::get('/subscribers/verify/{subscriber}', [SubscriberController::class, 've
     ->middleware('signed')
     ->name('subscribers.verify');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
